62a11020eca3d056dc654030d9144244979cce4e
Connor Ratcliffe <connor.ratcliffe@softwire.com>
2019-08-27 10:45:07 +0100

    Build log variable during execution.

--- mongo-to-azure/run.ps1
+++ mongo-to-azure/run.ps1
@@ -2,7 +2,15 @@ using namespace System.Net;
 
 param($Timer); # Receive Timer from input binding
 
-### Prepare to dump & zip ###
+$script:log = "";
+
+function Write-Log {
+	param( $line );
+	$script:log += "$line`r`n";
+	Write-Host $line;
+}
+
+### Prepare to dump and zip ###
 $mongodumpPath = "$env:HOME\site\wwwroot\mongo-to-azure\mongodump.exe";
 $dumpName = "dump";
 $dumpPath = "$env:HOME\$dumpName";
@@ -12,33 +20,41 @@ $zipName = "$formattedDate.zip";
 $zipPath = "$env:HOME\$zipName";
 #############################
 
-### Dump & zip ##############
-Write-Host "Started Dumping to $dumpPath";
-& $mongodumpPath /h $env:Host /d $env:Database /u $env:User /p $env:Password /o $dumpPath --quiet;
-Write-Host "Finished dumping";
+### Dump ####################
+Write-Log -line "Started dumping to $dumpPath.";
+$errorMessage = & $mongodumpPath /h $env:Host /d $env:Database /u $env:User /p $env:Password /o $dumpPath --quiet;
+if ($LASTEXITCODE -ne 0) # If mongodump didn't succeed
+{
+    throw $errorMessage;
+}
+Write-Log -line "Finished dumping.";
+#############################
 
-Write-Host "Started zipping to $zipPath";
+### Zip #####################
+Write-Log -line "Started zipping to $zipPath.";
 Add-Type -Assembly "System.IO.Compression.FileSystem";
 [System.IO.Compression.ZipFile]::CreateFromDirectory($dumpPath, $zipPath);
-Write-Host "Finished zipping";
+Write-Log -line "Finished zipping.";
 #############################
 
 #### Upload #################
 $storageAccountName = "mongotoazure";
 $containerName = "mongo-to-azure-container";
 
-Write-Host "Started creating storage context for $storageAccountName";
+Write-Log -line "Started creating storage context for $storageAccountName.";
 $storageContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $env:StorageAccountKey;
-Write-Host "Finished creating storage context";
+Write-Log -line "Finished creating storage context.";
 
-Write-Host "Started uploading blob to $containerName";
-[void](Set-AzStorageBlobContent -Container $containerName -File $zipPath -Blob $zipName -Context $storageContext); # Don't log the result
-Write-Host "Finished uploading blob";
+Write-Log -line "Started uploading blob to $containerName.";
+[void](Set-AzStorageBlobContent -Container $containerName -File $zipPath -Blob $zipName -Context $storageContext); # Don't output the result
+Write-Log -line "Finished uploading blob.";
 #############################
 
 ### Clean ###################
+Write-Log -line "Removing dump and zip.";
 Remove-Item $dumpPath -Recurse;
 Remove-Item $zipPath;
+Write-Log -line "Removed dump and zip.";
 #############################
 
 ### Build email #############
@@ -49,11 +65,11 @@ $shouldGenerateDownloadLink = $currentDayOfTheWeek -eq $downloadLinkDayOfTheWeek
 $subject = "MongoDB Backup $formattedDate";
 $content = "The MongoDB database ($env:Database) has been backed up to Azure Blob Storage."
 
-If($shouldGenerateDownloadLink) {
-    Write-Host "Started creating SAS token";
+if ($shouldGenerateDownloadLink) {
+    Write-Log -line "Started creating SAS token.";
     $expiryDate = (Get-Date).AddHours($env:DownloadLinkLifespanInHours);
     $blobUri = New-AzStorageBlobSASToken -Container $containerName -Blob $zipName -Context $storageContext -Permission r -ExpiryTime $expiryDate -FullUri;
-    Write-Host "Finished creating SAS token";
+    Write-Log -line "Finished creating SAS token.";
     $content += "The backup can be downloaded from $blobUri until $expiryDate.";
 }
 
@@ -70,7 +86,7 @@ $header = @{"Authorization" = "Bearer $env:SendGridAPIKey"; "Content-Type" = "ap
 #############################
 
 ### Send email ##############
-Write-Host "Started sending email";
-[void](Invoke-RestMethod -Uri https://api.sendgrid.com/v3/mail/send -Method Post -Headers $header -Body $body); # Don't log the result
-Write-Host "Finished sending email";
+Write-Log -line "Started sending email.";
+[void](Invoke-RestMethod -Uri https://api.sendgrid.com/v3/mail/send -Method Post -Headers $header -Body $body); # Don't output the result
+Write-Log -line "Finished sending email.";
 #############################
